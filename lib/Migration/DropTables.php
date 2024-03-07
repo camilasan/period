e447617bbedcece15602ec47dfd78f95f99deafc
Camila Ayres <hello@camilasan.com>
2024-03-07 18:36:09 +0100

    TEST: drop all tables.
    Signed-off-by: Camila Ayres <hello@camilasan.com>

---
+++ lib/Migration/DropTables.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @copyright Copyright (c) 2024 Camila Ayres <hello@camilasan.com>
+ *
+ * @author Camila Ayres <hello@camilasan.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Period\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class DropTables extends SimpleMigrationStep
+{
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     * @return null|ISchemaWrapper
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options)
+    {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if (!$schema->hasTable('period_profile')) {
+            $table = $schema->delete('period_profile');
+        }
+
+        if (!$schema->hasTable('period_category')) {
+            $table = $schema->delete('period_category');
+        }
+
+        if (!$schema->hasTable('period_symptoms')) {
+            $table = $schema->delete('period_symptom');
+        }
+
+        if (!$schema->hasTable('period_calendar')) {
+            $table = $schema->delete('period_calendar');
+        }
+
+        return $schema;
+    }
+}
